0870533ca62b41a5e333bffc527c59051caec2ac
Sijal Shrestha <42118868+CleverGirl1@users.noreply.github.com>
2024-03-22 15:59:11 -0400

    Pushed streamlit app py file

---
+++ app.py
@@ -0,0 +1,175 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import joblib
+from tld import get_tld, is_tld
+from urllib.parse import urlparse
+import re
+from googlesearch import search
+import tldextract
+from tldextract import extract as tld_extract
+from tld import get_tld, is_tld
+from tld.exceptions import TldDomainNotFound, TldBadUrl, TldIOError
+import hashlib
+import whois
+import warnings
+
+
+
+    
+def get_url_length(url):
+    return len(url)
+
+def get_domain(url):
+    try:
+        res = get_tld(url, as_object=True, fail_silently=False, fix_protocol=True)
+        domain = res.parsed_url.netloc
+    except :
+        domain = None
+    return domain
+
+def get_abnormal_url(url):
+    hostname = urlparse(url).hostname
+    hostname = str(hostname)
+    match = re.search(hostname, url)
+    if match:
+        # print match.group()
+        return 1
+    else:
+        # print 'No matching pattern found'
+        return 0    
+    
+def get_having_ip_address(url: str) -> int:
+    match = re.search(
+        '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
+        '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  # IPv4
+        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)' # IPv4 in hexadecimal
+        '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # Ipv6
+    if match:
+        # print match.group()
+        return 1
+    else:
+        # print 'No matching pattern found'
+        return 0
+    
+def get_sum_count_special_characters(url: str) -> int:
+    special_chars = ['@','?','-','=','.','#','%','+','$','!','*',',','//']
+
+    num_special_chars = sum(char in special_chars for char in url)
+    return num_special_chars
+
+def get_httpSecured(url: str) -> int:
+    htp = urlparse(url).scheme
+    match = str(htp)
+    if match == 'https':
+        # print match.group()
+        return 1
+    else:
+        # print 'No matching pattern found'
+        return 0
+
+def get_digit_count(url: str) -> int:
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits + 1
+    return digits
+
+def get_letter_count(url: str) -> int:
+    letters = 0
+    for i in url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+
+
+def get_has_shortening_service(url):
+    pattern = re.compile(r'bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                         r'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                         r'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                         r'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                         r'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                         r'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                         r'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
+                         r'tr\.im|link\.zip\.net')
+    match = pattern.search(url)
+    return int(bool(match))
+def get_abnormal_url(url):
+    parsed_url = urlparse(url)
+    hostname = parsed_url.hostname
+    if hostname:
+        hostname = str(hostname)
+        match = re.search(hostname, url)
+        if match:
+            return 1
+    return 0
+def get_secure_http(url):
+    scheme = urlparse(url).scheme
+    if scheme == 'https':
+        return 1
+    else:
+        return 0
+def get_have_ip_address(url):
+    pattern = r'(([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.' \
+              r'([01]?\d\d?|2[0-4]\d|25[0-5])\/)|' \
+              r'(([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.' \
+              r'([01]?\d\d?|2[0-4]\d|25[0-5])\/)|' \
+              r'((0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\/)' \
+              r'(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}|' \
+              r'([0-9]+(?:\.[0-9]+){3}:[0-9]+)|' \
+              r'((?:(?:\d|[01]?\d\d|2[0-4]\d|25[0-5])\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d|\d)(?:\/\d{1,2})?)'
+
+    match = re.search(pattern, url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def get_google_index(url):
+    site = search(url, 5)
+    return 1 if site else 0
+
+
+
+model = joblib.load('classifier5_model.joblib')
+st.title('Is the URL a malware')
+url= st.text_input("Enter the url")
+
+def get_url(url):
+    url = url.replace('www.', '')
+    url_len = len(url)
+    letters_count = get_letter_count(url)
+    digits_count  = get_digit_count(url)
+    special_chars_count = get_sum_count_special_characters(url)
+    shortened = get_has_shortening_service(url)
+    abnormal = get_abnormal_url(url)
+    secure_https = get_httpSecured(url)
+    have_ip = get_having_ip_address(url)
+    index_google = get_google_index(url)
+    
+    parsed_url  = urlparse(url)
+    
+    return {
+        'url_len': url_len,
+        'letters_count': letters_count,
+        'digits_count': digits_count,
+        'special_chars_count': special_chars_count,
+        'shortened': shortened,
+        'abnormal': abnormal,
+        'secure_http': secure_https,
+        'have_ip': have_ip,
+        'GoogleIndex' : index_google
+    }
+
+
+def model1_predict(url):
+    numerical_values = get_url(url)
+    prediction_int = model.predict(np.array(list(numerical_values.values())).reshape(1, -1))[0]
+    if prediction_int == 0:
+        st.success('URL is not a malware')
+    else: 
+        st.error('URl is a malware') 
+    
+
+trigger = st.button('Predict', on_click=model1_predict(url))
